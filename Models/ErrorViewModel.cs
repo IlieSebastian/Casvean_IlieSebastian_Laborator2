5de5c187b568b55b1e1a8120fdcdeeef85853d99
Sebastian Casvean <iliecasv@yahoo.com>
2020-10-14 19:17:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Casvean_IlieSebastian_Laborator2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

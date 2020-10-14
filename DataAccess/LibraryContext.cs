9fc9c00185853d8ac52393e44fa61992b830b90f
Sebastian Casvean <iliecasv@yahoo.com>
2020-10-14 19:34:38 +0300

    -creare librarycontext cu metodele de get customers/orders/books

---
+++ DataAccess/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Casvean_IlieSebastian_Laborator2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Casvean_IlieSebastian_Laborator2.DataAccess
+{
+    public class LibraryContext:DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}

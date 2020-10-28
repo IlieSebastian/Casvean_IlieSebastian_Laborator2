cce88345c0d4efa3f4beef5192f03190b9a65c58
Sebastian Casvean <iliecasv@yahoo.com>
2020-10-28 20:44:46 +0200

    -testare baza de date noua pentru debug -nu se genereaza ID la publisher

---
+++ Migrations/20201028183147_ExtendedModel4.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Casvean_IlieSebastian_Laborator2.Migrations
+{
+    public partial class ExtendedModel4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
